caf2adc8a6f201b3bda199c206b84771b4d3d782
jcuello123 <jcuello123@yahoo.com>
2020-07-22 21:39:35 -0400

    implemented guessing game

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-require('dotenv').config();
-const fs = require('fs');
-const Discord = require('discord.js');
-const {prefix} = require('./config.json');
+require("dotenv").config();
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix } = require("./config.json");
 
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
-for (const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    bot.commands.set(command.name, command);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command);
 }
 
-bot.on('message', (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-    
-    if (!bot.commands.has(command)) message.reply('No such command, dingus.');
+bot.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (
+    !bot.commands.has(command) &&
+    commmand != "leave" &&
+    commmand != "skip" &&
+    commmand != "play" &&
+    commmand != "fs" &&
+    commmand != "join"
+  )
+    return message.reply("No such command, dingus.");
+  else {
     try {
-        bot.commands.get(command).execute(message, args);
+      bot.commands.get(command).execute(message, args);
     } catch (error) {
-        console.log(error);
-        message.reply('There was an error trying to execute that command. Check your code Jeff..');
+      console.log(error);
+      message.reply(
+        "There was an error trying to execute that command. Check your code Jeff.."
+      );
     }
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
