0d36d11dbe5233e5a9d5ab9325f439e8ba9f5947
jcuello123 <jcuello123@yahoo.com>
2022-02-17 23:13:57 -0500

    remove that command doesnt exist msg

--- index.js
+++ index.js
@@ -9,42 +9,42 @@ bot.commands = new Discord.Collection();
 
 //commands
 const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+	.readdirSync("./commands")
+	.filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  bot.commands.set(command.name, command);
+	const command = require(`./commands/${file}`);
+	bot.commands.set(command.name, command);
 }
 
 bot.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
 
-  if (
-    command === "leave" ||
-    command === "skip" ||
-    command === "play" ||
-    command === "fs" ||
-    command === "join"
-  ) {
-    return;
-  }
+	if (
+		command === "leave" ||
+		command === "skip" ||
+		command === "play" ||
+		command === "fs" ||
+		command === "join"
+	) {
+		return;
+	}
 
-  if (!bot.commands.has(command))
-    return message.reply("No such command, dingus.");
-  else {
-    try {
-      bot.commands.get(command).execute(message, args);
-    } catch (error) {
-      console.log(error);
-      message.reply(
-        "There was an error trying to execute that command. Check your code Jeff.."
-      );
-    }
-  }
+	if (!bot.commands.has(command)) {
+		return;
+	} else {
+		try {
+			bot.commands.get(command).execute(message, args);
+		} catch (error) {
+			console.log(error);
+			message.reply(
+				"There was an error trying to execute that command. Check your code Jeff.."
+			);
+		}
+	}
 });
 
 bot.login(process.env.BOT_TOKEN);
